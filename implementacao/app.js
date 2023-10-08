cc75c994d57a1c3d91a4897643c8f78aa7fa8c0d
Letícia Lott <103859841+0xLott@users.noreply.github.com>
2023-10-07 23:20:58 -0300

    Query examples added

--- implementacao/app.js
+++ implementacao/app.js
@@ -6,21 +6,7 @@ const supabaseKey = process.env.SUPABASE_KEY
   
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function queryTest() {
-    try {
-        let { data, error } = await supabase
-        .from('students')
-        .select('cpf');
 
-        if (error) {
-            throw error;
-        }
 
-        console.log('Data:', data);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-}
 
-queryTest();
 
