0416475bc6ef4c93e15a269b50f7f9f5bd30c3c7
Letícia Lott <103859841+0xLott@users.noreply.github.com>
2023-10-08 00:08:53 -0300

    Server setup

--- implementacao/app.js
+++ implementacao/app.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
+const express = require('express');
+const session = require('express-session');
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
   
 const supabase = createClient(supabaseUrl, supabaseKey)
+const app = express();
+const PORT = process.env.PORT;
 
+app.get("/", (req, res) => {
+    res.send("MGS");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
 
 
 
