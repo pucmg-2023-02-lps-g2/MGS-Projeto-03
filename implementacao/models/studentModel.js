6f4dd271c5cb6dcf17a642c5963c540b80550ea6
Drumond <drumond.work@gmail.com>
2023-11-26 17:06:22 -0300

    metodo removeCoins refatorado.

--- implementacao/models/studentModel.js
+++ implementacao/models/studentModel.js
@@ -32,89 +32,78 @@ async function getPersonEmailByUUID(uuid) {
 }
 
 async function removeCoins(req, res) {
-    return new Promise(async (resolve, reject) => {
-    
-        const { price, id } = req.body
+    const { price, id } = req.body;
+    const { cpf, email, name } = req.cookies;
 
-        const { cpf, email, name } = req.cookies
+    try {
+        const benefit = await getBenefitById(id);
+        const partner = await getPartnerById(benefit[0].partner_id);
+        const partnerPerson = await getPersonByCpf(partner[0].cpf);
+        const studentBalance = await getBalanceByCpf(cpf);
 
-        try {
+        checkBalance(studentBalance, price, res);
 
-            const benefit = await getBenefitById(id)
+        await updateBalance(cpf, studentBalance, price);
 
-            const partner = await getPartnerById(benefit[0].partner_id)
+        const message = `- ${Number(price)} Moedas - Resgatou ${benefit[0].name}`;
+        await createNewTransaction(cpf, message);
 
-            const partnerPerson = await getPersonByCpf(partner[0].cpf)
+        const cupomId = crypto.randomUUID();
 
-            const studentBalance = await getBalanceByCpf(cpf)
+        await sendEmailToStudent(email, benefit, cupomId);
+        await sendEmailToPartner(partnerPerson.email, name, benefit, cupomId);
 
-            if (Number(studentBalance[0].balance) - Number(price) < 0) {
-                res.render('error', { message: 'Você não tem moedas suficientes para resgatar esse benefício' })
+        res.render('redeemed', { benefit: benefit[0], balance: studentBalance });
 
-                return
-            }
-
-            await supabase.from('person').update({ balance: (Number(studentBalance[0].balance) - Number(price)) }).eq('cpf', cpf)
-
-            const message = `- ${Number(price)} Moedas - Resgatou ${benefit[0].name}`
-
-            await createNewTransaction(cpf, message)
-
-            const cupomId = crypto.randomUUID()
-
-            const msgStudent = {
-                to: email.replace(/%40/g, "@"),
-                from: '1396722@sga.pucminas.br',
-                subject: 'MGS - Cupom para Resgate de Beneficio Presencial',
-                text: 'Cupom MGS',
-                html: `
-                    <div>
-                        <p>Obrigado por resgatar o beneficio <strong>${benefit[0].name}!</strong> Geramos um cupom para que você possa resgata-lo presencialmente: </p>
-                        <p>Cupom: <strong>${cupomId}</strong></p>
-                    <div>`,
-            }
-
-            const msgPartner = {
-                to: partnerPerson.email,
-                from: '1396722@sga.pucminas.br',
-                subject: 'MGS - Beneficio Resgatado',
-                text: 'Cupom MGS',
-                html: `
-                    <div>
-                        <p>A estudante <strong>${name.replace(/%40/g, " ")}</strong> resgatou o beneficio <strong>${benefit[0].name}</strong> e um cupom foi gerado: </p>
-                        <p>Cupom: <strong>${cupomId}</strong></p>
-                    </div>`,
-            }
-
-            await sgMail
-                .send(msgStudent)
-                .then(() => {
-                    console.log('Email sent to student: ' + msgStudent.to)
-                })
-                .catch((error) => {
-                    console.error(error)
-                })
-
-            await sgMail
-                .send(msgPartner)
-                .then(() => {
-                    console.log('Email sent to partner: ' + msgPartner.to)
-                })
-                .catch((error) => {
-                    console.error(error)
-                })
-
-            res.render('redeemed', { benefit: benefit[0], balance: studentBalance })
-
-            resolve({ status: 200 })
-
-        } catch (error) {
-
-            reject({ status: 500 })
-        }
-
-    })
+        return { status: 200 };
+    } catch (error) {
+        throw new Error(`Failed to remove coins: ${error}`);
+    }
 }
+
+function checkBalance(studentBalance, price, res) {
+    if (Number(studentBalance[0].balance) - Number(price) < 0) {
+        res.render('error', { message: 'Você não tem moedas suficientes para resgatar esse benefício' });
+        throw new Error('Insufficient balance');
+    }
+}
+
+async function updateBalance(cpf, studentBalance, price) {
+    await supabase.from('person').update({ balance: (Number(studentBalance[0].balance) - Number(price)) }).eq('cpf', cpf);
+}
+
+async function sendEmailToStudent(email, benefit, cupomId) {
+    const msgStudent = {
+        to: email.replace(/%40/g, "@"),
+        from: '1396722@sga.pucminas.br',
+        subject: 'MGS - Cupom para Resgate de Beneficio Presencial',
+        text: 'Cupom MGS',
+        html: `
+            <div>
+                <p>Obrigado por resgatar o beneficio <strong>${benefit[0].name}!</strong> Geramos um cupom para que você possa resgata-lo presencialmente: </p>
+                <p>Cupom: <strong>${cupomId}</strong></p>
+            <div>`,
+    };
+
+    await sgMail.send(msgStudent);
+}
+
+async function sendEmailToPartner(email, name, benefit, cupomId) {
+    const msgPartner = {
+        to: email,
+        from: '1396722@sga.pucminas.br',
+        subject: 'MGS - Beneficio Resgatado',
+        text: 'Cupom MGS',
+        html: `
+            <div>
+                <p>A estudante <strong>${name.replace(/%40/g, " ")}</strong> resgatou o beneficio <strong>${benefit[0].name}</strong> e um cupom foi gerado: </p>
+                <p>Cupom: <strong>${cupomId}</strong></p>
+            </div>`,
+    };
+
+    await sgMail.send(msgPartner);
+}
+
 
 async function giveCoins(req, res) {
     return new Promise(async (resolve, reject) => {
